368a481c7ad08ba81667efb0bb32111ea24dce63
kaleabbyh <kaleabbyh@gmail.com>
2023-03-29 11:16:52 +0300

    tutor model controller and routes are created

--- src/tutee/pages/courses/AddCourse.jsx
+++ src/tutee/pages/courses/AddCourse.jsx
@@ -104,31 +104,6 @@ const AddCourse = () => {
           </defs>
         </svg>
 
-        {/* <svg
-          viewBox="0"
-          aria-hidden="true"
-          className="absolute left-1/2 -top-52 -z-10 w-[68.5625rem] -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
-        >
-          <path
-            fill="url(#8ddc7edb-8983-4cd7-bccb-79ad21097d70)"
-            fillOpacity=".2"
-            d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z"
-          />
-          <defs>
-            <linearGradient
-              id="8ddc7edb-8983-4cd7-bccb-79ad21097d70"
-              x1="1097.04"
-              x2="-141.165"
-              y1=".22"
-              y2="363.075"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#776FFF" />
-              <stop offset={1} stopColor="#FF4694" />
-            </linearGradient>
-          </defs>
-        </svg> */}
-
         <Container maxWidth="lg">
           <Box py={2}>
             <Button
@@ -170,7 +145,7 @@ const AddCourse = () => {
                 flexWrap="wrap"
                 sx={{ boxShadow: 1, p: 3, borderRadius: "4%" }}
               >
-                {courses.length ? (
+                {courses?.length ? (
                   <>
                     {courses.map((course, index) => (
                       <Box m={1} key={course._id}>
