3bb570005d65c6ee465527096a24ddb6f97cf882
kaleabbyh <kaleabbyh@gmail.com>
2023-04-03 18:01:14 +0300

    feat update profile and view profile updated

--- src/tutee/pages/users/UpdateProfile.jsx
+++ src/tutee/pages/users/UpdateProfile.jsx
@@ -94,7 +94,7 @@ const UpdateProfile = () => {
 
           <div className="my-5 px-6">
             <Link
-              to="#"
+              to="/tutee/profile/viewprofile"
               className="text-gray-200 block rounded-lg text-center font-medium leading-6 px-6 py-3 bg-indigo-600 hover:bg-indigo-700 hover:text-white"
             >
               welcome {tutee.fname} {tutee.lname}
