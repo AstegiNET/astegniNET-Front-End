8c8b03cd4df380d1bbdd1d4322739cfb940baf5e
kaleabbyh <kaleabbyh@gmail.com>
2023-04-05 15:22:53 +0300

    updated

--- src/videoCall/VideoCall.jsx
+++ src/videoCall/VideoCall.jsx
@@ -91,6 +91,7 @@ const VideoCall = () => {
   return (
     <>
       <Header />
+      {/* header added */}
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">AstegniNET</p>
