705768404640f339bb1541c448b75df406bba1db
kaleabbyh <kaleabbyh@gmail.com>
2023-04-23 23:12:49 +0300

    frontend updated

--- src/tutee/pages/users/UpdateProfile.jsx
+++ src/tutee/pages/users/UpdateProfile.jsx
@@ -282,7 +282,7 @@ const UpdateProfile = () => {
                     </div>
                   </div>
                 </div>
-                <div className="mt-5">
+                <div className="my-10">
                   <button
                     type="submit"
                     className="mb-5 block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
