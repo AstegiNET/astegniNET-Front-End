8e3b52de4536fc9dcc373c9bd9770098edea91e3
fikireab-m <fikireab.meku@gmail.com>
2023-06-23 15:49:11 +0300

    clean up

--- src/tutor/components/alerts/InfoAlert.jsx
+++
@@ -1,6 +0,0 @@
-export default function InfoAlert({sharedLink}){
-    return navigator.clipboard.writeText(sharedLink).then(() => {
-        // Display an alert box with a message
-        window.alert("Link copied to clipboard!");
-      });
-}
