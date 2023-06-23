76ddc1af529a1bf079a52aeb9ad422da51e02d28
fikireab-m <fikireab.meku@gmail.com>
2023-06-23 13:17:41 +0300

    minor fix

--- src/tutee/pages/users/Login.jsx
+++ src/tutee/pages/users/Login.jsx
@@ -122,6 +122,8 @@ const Login = () => {
                   required
                   onChange={onChange}
                   placeholder="enter email"
+                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                  title="email should follow a valid email address format"
                   autoComplete="email"
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -143,6 +145,8 @@ const Login = () => {
                   required
                   onChange={onChange}
                   placeholder="enter password"
+                  pattern="[A-Za-z]{3,20}"
+                  title="name should be in alphabets and at least 3 characters long"
                   autoComplete="password"
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
