e3b3428f3ed8c6cdf1b13304d9910a5792a6fcb4
kaleabbyh <kaleabbyh@gmail.com>
2023-07-06 13:25:10 +0300

    frontend update

--- src/tutee/pages/users/Register.jsx
+++ src/tutee/pages/users/Register.jsx
@@ -246,8 +246,8 @@ const Register = () => {
                   required
                   onChange={onChange}
                   placeholder="enter password"
-                  pattern=".{6,10}"
-                  title="Password must be at least 6, not more than 10 characters long"
+                  pattern=".{3,10}"
+                  title="Password must be at least 3, not more than 10 characters long"
                   autoComplete="password"
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -271,8 +271,8 @@ const Register = () => {
                   autoComplete="password2"
                   onChange={onChange}
                   placeholder="enter password"
-                  pattern=".{6,10}"
-                  title="Password must be at least 6, not more than 10 characters long"
+                  pattern=".{3,10}"
+                  title="Password must be at least 3, not more than 10 characters long"
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
